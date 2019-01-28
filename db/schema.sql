06b9ef462644e8a8cf5e0fd839d24cec5fabb62d
Mark Langhamer <langhamerm@gmail.com>
2019-01-28 13:09:37 -0600

    schema fix

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,10 @@ USE burgers_db;
 
 
 -- Table Creation
-CREATE TABLE burgers(
-id INTEGER AUTO_INCREMENT PRIMARY KEY,
-burger_name VARCHAR(50),
-devoured BOOLEAN,
-date TIMESTAMP);
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+  	PRIMARY KEY(id)
+);
